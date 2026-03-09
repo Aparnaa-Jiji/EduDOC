d0f475d057e43727f5c938063bd5cfa3a09c083e
Manjima <manjima.msunil2@gmail.com>
2026-03-09 10:09:43 +0530

    Fix ALLOWED_HOSTS syntax

--- edudoc/settings.py
+++ edudoc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5jj01o%urf&b!jwv#ezwc61sc0d7f0ykfaf%vgx9c68y_=c$gm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
